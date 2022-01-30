bde805fd46864c4ca444f1d6003e3ac4a777b4e5
Rajvardhan Sawant <98679121+RajSawant2158@users.noreply.github.com>
2022-01-30 20:42:31 +0530

    Add files via upload

---
+++ java 1/src/com/raj/Main.java
@@ -0,0 +1,19 @@
+// Write a C program to perform input/output of all basic data types.
+package com.raj;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=10;
+        float b=2.5f;
+        double c=2.2;
+        char d;
+        double e;
+        System.out.println(e = a+b+c );
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+}
